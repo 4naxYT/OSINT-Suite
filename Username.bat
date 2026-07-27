990c1efe74e71e7f8cc523b29e149f3b756afdeb
4naxYT <banerjee.pinaki@gmail.com>
2026-07-27 18:04:33 +0530

    Add files via upload

---
+++ Username.bat
@@ -0,0 +1,19 @@
+@echo off
+echo.
+echo Downloading Sherlock....
+python -m pip install --user sherlock-project
+cls
+echo.
+echo Downloading Sherlock....
+echo Completed
+echo Adding to PATH...
+set PATH=%PATH%;%APPDATA%\Python\Python314\Scripts
+sherlock --version
+echo.
+echo Completed
+cls
+set /p usr=Exter Username to Reverse Search :  
+echo.
+echo Running Checks On %usr%...
+sherlock %usr% --nsfw --no-txt --print-found 
+pause
